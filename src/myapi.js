239575c39215d4c0c4b1021f95c43a8de26c5cd6
littlegamer757 <jneissl27@gmail.com>
2021-02-27 11:39:03 +0100

    Update package.json

--- src/myapi.js
+++ src/myapi.js
@@ -4,17 +4,18 @@ const app = express();
 
 app.get("/", (req, res) => {
     res.send("<h1>Seas Jungs</h1>");
-})
+});
 
 app.get("/light/:id/:state", (req, res) => {
     const result = lightHandler.handleLights(req.params.id, req.params.state);
     res.send(result);
-})
+});
 
 app.listen(3000, err => {
     if (err) {
         console.log("There was an error", err);
         return;
     }
-    console.log("Listening on port 3000")
+
+    console.log("Listening on port 3000");
 });
